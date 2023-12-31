af1fd1df62c15021b79cba4c7f8d2a3204f574e8
aluka <88232045+a1uka@users.noreply.github.com>
2023-12-31 19:26:02 +0300

    Update Matrix Det.cpp

--- Matrix Det/Matrix Det.cpp
+++ Matrix Det/Matrix Det.cpp
@@ -2,18 +2,26 @@
 #include <stdio.h>
 #include <time.h>
 #include <conio.h>
+//not working for matrix bigger then 2x2 at the moment
 
-void matrixToStep(const int* n, double* a) {
-	double m = *(a + 0);
-	for (int i = 0; i < *n; i++) {
-		*(a + i) = (*(a + i)) / m;
-	}
-	for (int i = 1; i < *n; i++) {
-		for (int j = 1; j < *n; j++) {
-			*(a + 100 * i + j) -= (*(a + 0 + j)) * (*(a + 100 * i + 0));
+double matrixToStep(const int* n, double* a) {
+	double mult = 1, m, a_i;
+	for (int k = 0; k < *n - 1; k++) {
+		m = *(a + k*100 + k);
+		mult = mult / m;
+		
+		for (int i = k; i < *n; i++) {
+			*(a + 100*(k) + i) = (*(a + 100 * (k) + i)) / m;
 		}
-	}
+		for (int i = k + 1; i < *n; i++) {
+			a_i = *(a + 100 * i + k);
+			for (int j = 0; j < *n; j++) {
+				*(a + (100) * i + j) = (*(a + (100) * i + j)) - a_i * (*(a + (100) * k + j));
+			}
+		}
 
+	}
+	return mult;
 }
 
 void enterMatrix(const int* n, double* a) {
@@ -28,7 +36,7 @@ void enterMatrix(const int* n, double* a) {
 void printMatrix(const int* n, const double* a) {
 	for (int i = 0; i < *n; i++) {
 		for (int j = 0; j < *n; j++) {
-			printf("%6.2lf ", *(a + (100) * i + j));
+			printf("%8.2lf ", *(a + (100) * i + j));
 		}
 		puts("");
 	}
@@ -42,8 +50,12 @@ int main() {
 	enterMatrix(&n, *A);
 	puts("Output of the obtained matrix");
 	printMatrix(&n, *A);
-	matrixToStep(&n, *A);
+	double det = matrixToStep(&n, *A);
+	for (int i = 0; i < n; i++) {
+		det *= A[i][i];
+	}
 	puts("Stepped matrix");
 	printMatrix(&n, *A);
+	printf("Determenant of the matrix = %lf", det);
 	_getch();
 }
